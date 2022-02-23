15fc863585f98094f34c236a16c6e66edaf2f35b
Barış Demir <94130933+barisdemr@users.noreply.github.com>
2022-02-24 01:14:01 +0300

    Add files via upload

---
+++ turkish.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Barış Demir | Resmi Website</title>
+    <link rel="icon" href="assets/img/favicon/favicon.png">
+    <link rel="stylesheet" href="assets/css/main.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body >
+        <!--header-->
+        <div class="header-bg" style="background-image: url('assets/img/background/bg2.png');">
+    <header>
+        <div class="header-left">
+      <a href="" class="logo-a">  <img src="assets/img/logo/logo-white.png" alt="barisdemir.net Logo"></a>
+    </div>
+    <div class="header-right">
+    <ul>
+        <li><a href="#hakkimda">Hakkımda</a></li>
+        <li><a href="https://blog.barisdemir.net">Blog</a></li>
+        <li><a href="#yetenek">Yetenek</a></li>
+        <li><a href="#projeler">Projeler</a></li>
+        <li><a href="#iletisim">İletişim</a></li>
+    </ul>
+    <div class="language">
+        <a href="http://www.barisdemir.net/english"> <span class="language-img"><img src="assets/img/language/english.png" alt=""></span> English</a>
+    </div>
+    </div>
+</header>
+</div>
+<!--header end-->
+
+<div class="icerik">
+    <div class="ben">
+<div class="hakkimda-left">
+    <img src="assets/img/pp/profile-photo.jpg" alt="">
+</div>
+<div class="hakkimda-right">
+    <p class='merhaba'>Merhaba,</p>
+    <h1>Ben <span>Barış Demir</span></h1>
+    <p class='is'>Open to learning, Student, Front-end developer</p>
+    <div class="hakkimda">
+    <ul class="hakkimda-baslik">
+        <li>
+            <p>Doğum Tarihi</p>
+        </li>
+        <li>
+            <p>Konum</p>
+        </li>
+        <li>
+            <p>E-mail</p>
+        </li>
+        <li>
+            <p>Doğum Tarihi</p>
+        </li>
+
+    </ul>
+    <ul class="hakkimda-icerik">
+        <li>
+        <p>04 Temmuz 2004</p>
+        </li>
+        <li>
+        <p>Kocaeli/Türkiye</p>
+        </li>
+        <li>
+        <p>me@barisdemir.net</p>
+        </li>
+        <li>
+        <p>04.07.2004</p>
+        </li>
+        
+    </ul>
+
+</div>
+<div class="sosyal-medya">
+<ul>
+    <li><a href="https://www.instagram.com/barisdemirvr"><i class="fa-brands fa-instagram"></i></a></li>
+    <li><a href="https://www.twitter.com/barisdemirvr"><i class="fa-brands fa-twitter"></i></a></li>
+    <li><a href="https://www.discord.gg"><i class="fa-brands fa-discord"></i></a></li>
+    <li><a href="https://www.github.com/barisdemr"><i class="fa-brands fa-github"></i></a></li>
+    <li><a href="https://www.linkedin.com/in/bar%C4%B1%C5%9F-demir-73b149226/"><i class="fa-brands fa-linkedin-in"></i></a></li>
+    <li><a href="https://stackoverflow.com/users/17402208/bar%c4%b1%c5%9f-demir"><i class="fa-brands fa-stack-overflow"></i></a></li>
+</ul>
+</div>
+</div>
+    </div>
+</div>
+
+</body>
+</html>
